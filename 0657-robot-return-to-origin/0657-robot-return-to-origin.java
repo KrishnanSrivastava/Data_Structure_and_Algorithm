7f53798b2274a8544c6cb69b79afc21a30799cd7
KrishnanSrivastava <krishnansrivastava12@gmail.com>
2026-04-05 19:20:01 +0530

    Time: 8 ms (24.19%), Space: 45.4 MB (84.38%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -3,9 +3,9 @@ class Solution {
         int x = 0, y = 0;
         for (int i = 0; i < moves.length(); i++) {
             if (moves.charAt(i) == 'R') x++;
-            if (moves.charAt(i) == 'L') x--;
-            if (moves.charAt(i) == 'U') y++;
-            if (moves.charAt(i) == 'D') y--;
+            else if (moves.charAt(i) == 'L') x--;
+            else if (moves.charAt(i) == 'U') y++;
+            else y--;
         }
         if ((x == 0) && (y == 0)) return true;
         return false;
