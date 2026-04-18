2db3e0359b091e81328822f0df1ee330e5f9704f
KrishnanSrivastava <krishnansrivastava12@gmail.com>
2026-04-19 02:50:25 +0530

    Time: 58 ms (13.34%), Space: 47.4 MB (58.1%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int i = s.length();
-        int j = p.length();
-        int dp[][] = new int[i][j];
-        return rec (i-1,j-1,s,p,dp);
+        int sl = s.length();
+        int pl = p.length();
+        boolean dp[][] = new boolean[sl + 1][pl + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < dp[0].length; j++) {
+            boolean flag = true;
+            for (int idx = 0; idx <= j - 1; idx++) {
+                if (p.charAt(idx) != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+
+                if (p.charAt(j - 1) == '?' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = (dp[i][j - 1] || dp[i - 1][j]);
+
+                } else
+                    dp[i][j] = false;
+            }
+        }
+
+        return dp[sl][pl];
     }
 
-    boolean rec(int i, int j, String s, String p, int[][] dp){
-        if (i < 0 && j < 0) return true;
-        if (j < 0) return false;
-        if (i < 0) {
-            for (int idx = 0; idx <= j; idx++){
-                if (p.charAt(idx) != '*') return false;
-            }
-            return true;
-        }
-        if (dp[i][j] != 0) return dp[i][j] == 1;
-        if (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i)){
-            dp[i][j] = (rec(i-1,j-1,s,p,dp))? 1:2;
-            return dp[i][j] == 1;
-        }
-        if (p.charAt(j) == '*'){
-            dp[i][j] = (rec(i,j-1,s,p,dp) || rec(i-1,j,s,p,dp))? 1:2;
-            return dp[i][j] == 1;
-        }
-        dp[i][j] = 2;
-        return false;
-    }
 }
