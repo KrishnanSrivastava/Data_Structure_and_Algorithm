43f246355abfeab8c6340c20a6afc42db1dac81a
KrishnanSrivastava <krishnansrivastava12@gmail.com>
2026-06-18 00:45:35 +0530

    Time: 1 ms (99.71%), Space: 47.3 MB (56.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,20 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return rec(nums, 0, dp);
-    }
-
-    int rec(int[] nums, int i, int[] dp) {
-        if (i >= nums.length - 1)
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-        int ans = 10000;
-        for (int j = 1; j <= nums[i]; j++) {
-            ans = Math.min(ans, 1 + rec(nums, i + j, dp));
+        int l = 0, r = 0, max = 0, jump = 0;
+        while(max < nums.length - 1){
+            for (int i = l; i <= r; i++){
+                max = Math.max(max,i+nums[i]);
+            }
+            jump++;
+            l = r+1;
+            r = max;
         }
-        return dp[i] = ans;
+        return jump;
     }
 }
